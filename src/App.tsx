8b4bf00d5a15a84e3fb8febb5b0f8ad1db59ff5e
TomasMatusek <Tomas.Matusek@edu.fh-kaernten.ac.at>
2024-01-29 11:57:44 +0100

    Create components & add currecnt pages from layout, add-load pages from component to the App.tsx and render

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import FooterPage from './components/FooterPage';
+import MainPage from './components/MainPage';
+import Navbar from './components/Navbar';
 import './index.css';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <MainPage />
+      <FooterPage />
+    </>
+  );
 }
 
 export default App;
